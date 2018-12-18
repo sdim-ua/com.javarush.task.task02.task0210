f42c3410891de4ddf93ad58fb8f66c87ba763920
sdim-ua <ingersol@yandex.ua>
2018-12-18 15:45:00 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+/* 
+Необъективная реальность
+*/
+
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Будешь плохо кодить, придет Java и съест твою память");
+    }
+}
